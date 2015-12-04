9709237baceb6bf6e451a72363501378c74c1d09
Joshua Hughes <email@joshua34.com>
2015-12-04 15:55:28 -0800

    updated file extension

--- weedMaps.js
+++ weedMaps.js
@@ -1,6 +1,6 @@
 var w = jQuery( document ).width();
 var diframe = '<iframe src="https://weedmaps.com/dispensaries/rocky-mountain-cannabis-2/menu/embed/" allowfullscreen webkitallowfullscreen mozallowfullscreen width="1170" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>';
-var miframe = '<iframe src="https://mobile.weedmaps.com/#/dispensaries/rocky-mountain-cannabis-2/menu" allowfullscreen webkitallowfullscreen mozallowfullscreen width="1170" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>';
+var miframe = '<iframe src="https://mobile.weedmaps.com/#/dispensaries/rocky-mountain-cannabis-2/menu" allowfullscreen webkitallowfullscreen mozallowfullscreen frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>';
 
 jQuery( document ).ready(function( $ ) {
 if ( w < 768 ) {
