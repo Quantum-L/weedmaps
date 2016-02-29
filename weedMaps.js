16f4d79591432fe32f8c509784c8ec0f77a7ee20
Joshua Hughes <email@joshua34.com>
2016-02-28 17:04:14 -0800

    Updated

--- weedMaps.js
+++ weedMaps.js
@@ -3,7 +3,7 @@ var diframe = '<iframe src="https://weedmaps.com/dispensaries/rocky-mountain-can
 var miframe = '<iframe src="https://mobile.weedmaps.com/#/dispensaries/rocky-mountain-cannabis-2/menu" allowfullscreen webkitallowfullscreen mozallowfullscreen width="1170" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>';
 
 jQuery( document ).ready(function( $ ) {
-if ( w < 768 ) {
+if ( w < 769 ) {
  $( "#storeMenu" ).append( $( miframe ) );
 }
 else {
